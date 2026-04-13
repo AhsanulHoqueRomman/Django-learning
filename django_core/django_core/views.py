b6751770d7b7709b948973d8765f7b40f86e4369
AhsanulHoqueRomman <hoque15-5330@diu.edu.bd>
2026-04-14 03:20:28 +0600

    Django  if else statement

--- django_core/django_core/views.py
+++ django_core/django_core/views.py
@@ -6,6 +6,7 @@ def home(request):
         "title": "Home",
         "heading": "Welcome to Our Landing Page",
         "clist": ["Python","Java","Django"],
+        "numbers": [10,20,30,40,50,60,70,80],
         "student_details": [
             {"name":"Romman", "Contact": 12345678},
             {"name":"Ahsanul", "Contact": 987654321}
